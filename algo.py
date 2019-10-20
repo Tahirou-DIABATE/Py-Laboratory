2111d1b2b270dc91b1f1c6dd73a08fd12437f8a2
Tahirou DIABATE <tahiroudiabate2@gmail.com>
2019-10-20 22:01:18 +0000

    Still learning

--- algo.py
+++ algo.py
@@ -1,3 +1,30 @@
+#
+#   Détermine si un nombre est premier ou non
+#
+def isPrime(nbr):
+  cpt = 0
+  for i in range(1, nbr):
+    if nbr%i is 0:
+      cpt += 1
+      if cpt is 2:
+        print(nbr, "N'est pas premier")
+        break
+  else:
+    print(nbr, "Est premier")
+    print(cpt)
+
+#
+# Détermine les nombres de la suite de fib inferieur à nbr
+#
+def fibonacci(nbr):
+  a, b = 0, 1
+  while a < nbr:
+    print(a, end=' ')
+    a, b = b, a+b
+
+#
+#   La fonction principale "main"
+#
 def main():
   liste = [6, 4, 2, 7, 3, 5, 1, 7, 9, 1]
 
@@ -6,7 +33,7 @@ def main():
     if item[0] in [2, 6]:
       liste[item[0]]= 0
 
-  print(sum(liste))
+  fibonacci(sum(liste))
   pass
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+def fibonacci(nbr):
+  a, b = 0, 1
+  while a < nbr:
+    print(a, end=' ')
+    a, b = b, a+b
+
 def main():
-  liste = [6, 4, 2, 7, 3, 5, 1, 7, 9, 1]
-
-  print(liste)
-  for item in enumerate(liste):
-    if item[0] in [2, 6]:
-      liste[item[0]]= 0
-
-  print(sum(liste))
+  fibonacci(17)
   pass
 
+
 if __name__ == "__main__":
-  main()
+    main()
